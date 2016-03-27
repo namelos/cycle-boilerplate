b0eba9bd1b170adfb1d613e121431adb86ebc0a0
namelos <namelos@outlook.com>
2016-03-27 16:09:30 +0800

    seprate effects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import { Observable } from 'rx'
 const { timer } = Observable
 
+/* --- logic --- */
+
+const main = () => ({
+  DOM: timer(0, 1000).map(i => `seconds elapsed ${i}`),
+  Log: timer(0, 2000).map(i => 2 * i)
+})
+
+/* --- effects --- */
+const DOMEffect = text$ =>
+  text$.subscribe(text =>
+    document.querySelector('#app').textContent = text)
+
+const consoleLogEffect = msg$ =>
+  msg$.subscribe(console::console.log)
+
+const effectsFunctions = {
+  DOM: DOMEffect,
+  Log: consoleLogEffect
+}
+
+/* --- binding --- */
+
+const run = (mainFn, effects) => {
+  const sink = mainFn()
+  Object.keys(effects).map(key =>
+    effects[key](sink[key]))
+}
+
+/* --- run --- */
+
+run(main, effectsFunctions)
